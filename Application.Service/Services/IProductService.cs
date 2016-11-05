5408c9c058754bf947955264d85787729ddb351a
sampanna pokhrel <sampanna99@gmail.com>
2016-11-04 23:59:35 -0500

    beginning

---
+++ Application.Service/Services/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using Application.Data;
+using System.Collections.Generic;
+
+namespace Application.Service.Services
+{
+    public interface IProductService
+    {
+        List<Product> GetProducts();
+        Product GetProduct(int id);
+
+    }
+}
---
+++ Application.Service/Services/ProductService.cs
@@ -0,0 +1,22 @@
+﻿using Application.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Application.Service.Services
+{
+    public class ProductService : IProductService
+    {
+        private ProductEntities db = new ProductEntities();
+
+        public Product GetProduct(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetProducts()
+        {
+            return db.Products.ToList();
+        }
+    }
+}
